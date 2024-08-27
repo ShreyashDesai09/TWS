24a0c24ba5b2838ddfe5e46e66791e634d185125
Ubuntu <ubuntu@ip-172-31-32-160.ec2.internal>
2024-08-27 20:37:42 +0000

    2024-08-27-20-37-42

---
+++ git_push.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+$cd /home/ubuntu/TWS_Study_material
+
+
+echo "THSI IS THE GIT STATUS"
+echo "$(git status)"
+if [ $? -eq 0 ]; then
+        echo "----------DONE----------"
+else
+        echo "----------ERROR----------"
+fi
+
+echo "GIT ADD STARTED"
+$(git add .)
+if [ $? -eq 0 ]; then
+        echo "----------DONE----------"
+else
+        echo "----------ERROR----------"
+fi
+
+echo "GIT COMMIT STARTED"
+echo "$(git commit -m "$(date '+%Y-%m-%d-%H-%M-%S')")"
+if [ $? -eq 0 ]; then
+        echo "----------DONE----------"
+else
+        echo "----------ERROR----------"
+fi
+
+echo "GIT PUSH STARTED"
+$(git push -u origin main)
+
