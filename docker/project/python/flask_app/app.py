baf18cf356ff9da9d99ea06cbdd5e7a72b13648e
ShreyashDesai09 <shreyashdesai31796@gmail.com>
2024-08-29 14:49:03 +0000

    2024-08-29-14-49-03

---
+++ docker/project/python/flask_app/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('api/status', methods=['GET'])
+def status():
+    return jsonify({"message": "server is up and running"})
+
+if __name__ = '__main__':
+    app.run(debug=TRUE)
